9fd87d684883b792760301a099aeb4ae5930740b
JakaV6 <108479524+JakaV6@users.noreply.github.com>
2022-07-09 09:21:55 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['66855048169','79999029953'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['66855048169'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '66855048169' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-  global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'Nan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.botname = 'Sakkarin-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.footer = 'Nan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-  global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-  global.myweb = 'https://api-riychdwayne.herokuapp.com'
-  global.region = 'indonesia'
+  global.youtube = 'https://youtu.be/lBN5szPGGDA'
+  global.mygc = 'https://chat.whatsapp.com/LX22xQnDb0yJijocPMYf2P'
+  global.myweb = 'https://Instagram.com/itsme_nitsu'
+  global.region = 'Thailand🇹🇭'
 
   // Set Wm
-  global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.packname = '© Sakkarin-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.author = 'Nan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
