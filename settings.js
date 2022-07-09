91398a2d1fa33e836595b821c6cd879d8c3c6155
JakaV6 <108479524+JakaV6@users.noreply.github.com>
2022-07-09 09:02:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,19 +22,19 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['66855048169'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['66855048169'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '66855048169' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Nan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Sakkarin-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Nan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
+global.youtube = 'https://youtu.be/lBN5szPGGDA'
+global.ig = 'https://instagram.com/itsme_nitsu'
 global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
 global.myweb = 'https://s.id/chikabot'
 global.email = 'riychdwaynee@gmail.com'
